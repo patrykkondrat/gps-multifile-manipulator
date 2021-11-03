8975c1ec191b0029bec912d8ec81e0853553dc99
Patryk <dr34mer00@gmail.com>
2021-11-03 22:45:38 +0100

    cmt 5

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import subprocess
 import os, sys
 from files import Filegetter
+
+
 path = 'C:/STATYKA BIAŁYSTOK/Łomża/POMIAR/RINEX/15.10'
 f = Filegetter(path)
 a = f.get_list_of(with_sufix='.21O')
-print(a[0])
-print(os.listdir(path))
+print(a)
+
 f._21o2rnx(a[0])
-b = f.get_list_of(with_sufix='.rnx')
+print(os.listdir(path))
+b = f.get_list_of(with_sufix='.rnx') 
 print(b)
-c = f._rnx2version2(b[0])
+c = f._rnx2version2(b[0]) # doesn't work
 
   
